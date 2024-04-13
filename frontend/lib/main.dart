419ddb34a7aecd5a9e1af007659a45a08592a39f
Bastien <sondruska@email.cz>
2024-04-13 10:20:33 +0200

    comit test

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -117,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Sobek',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
