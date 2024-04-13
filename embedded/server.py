d33a33a694c40f7161431da8c477bb3179b1705b
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2024-04-13 17:28:59 +0200

    added saving to db

--- embedded/server.py
+++ embedded/server.py
@@ -1,4 +1,5 @@
 import serial
+import sqlite3
 
 ser = serial.Serial(
   port='/dev/ttyUSB0',
@@ -10,10 +11,21 @@ ser = serial.Serial(
 
 print("connected to: " + ser.portstr)
 count=1
+db = sqlite3.connect('data.db')
+cursor = db.cursor()
+# check if the table exists
+cursor.execute('''CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, did TEXT, sid TEXT, type TEXT, value TEXT, time TEXT)''')
+db.commit()
+db.close()
 
 while True:
   line = ser.readline()
   line = line.decode('utf-8').strip()  # Added strip() to remove trailing newline
   if line:  # Only print if line is not empty
+    db = sqlite3.connect('data.db')
+    cursor = db.cursor()
+    cursor.execute('''INSERT INTO data (did, sid, type, value, time) VALUES (?, ?, ?, ?, datetime('now'))''', line['did'], line['sid'], line['t'], line['state'])
+    db.commit()
+    db.close()
     print(f"{count}: {line}")
     count += 1
